556e59a81044ffd9e528a9e949085df8444fa32c
F2nn1K <leo.vdf3@gmail.com>
2025-07-17 15:44:32 -0400

    Configura projeto corretamente para GitHub Pages com imagens na pasta public

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -18,7 +18,7 @@ const Footer: React.FC = () => {
           <div className="md:col-span-2">
             <div className="inline-block mb-4">
               <img 
-                src="./Logotipo.png" 
+                src="/Logotipo.png" 
                 alt="Auto Posto Estrela D'Alva" 
                 className="h-12 md:h-14 lg:h-16 w-auto"
               />
